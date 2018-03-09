91c17b82819eeab80afdcea445dca528b6520c49
zt <253542831@qq.com>
2018-03-09 18:30:42 +0800

    初始化导入

---
+++ src/api/login.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+
+export function login(username, password) {
+  return request({
+    url: '/login',
+    method: 'post',
+    data: {
+      username: username,
+      password: password
+    }
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/info',
+    method: 'post',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/logout',
+    method: 'post'
+  })
+}
